44a05cbb87b45ef02512066c5c7bfae9524c9caa
Joshua Reed <JoshuaR@clearswift.com>
2019-10-30 21:47:42 +0000

    Added to definitions.go

---
+++ go/definitions.go
@@ -0,0 +1,21 @@
+// Variables, constants and data types
+const myConst = "This cannot change"
+var myString string = "Hello World!"
+var myInt int // Defaults to system default, based on system architecture
+var myInt8 int8
+var myInt16 int16
+var myInt32 int32
+var myInt64 int64
+var myUnsignedInt uint // and all above variants
+var myBool bool
+var myFloat float
+var myFloat32 float32
+var myFloat64 float64
+
+// Packages
+import "fmt"
+
+// Functions
+func myFunc() {
+	fmt.Println("Hello World!")
+}
