c1a587e8061236f87ee704446d749b13edf24a55
Joshua Reed <JoshuaR@clearswift.com>
2019-11-12 15:49:11 +0000

    Added playground.pp, work in rust

---
+++ rust/playground.rs
@@ -0,0 +1,6 @@
+printLn!("Hello world!"); // '!' indicates a macro, NOT a function
+// Variables:
+let foo = 5; // immutable
+let mut bar = 5; // mutable
+let mut answer = String::new(); // '::' indicates 'new' is assocated function of the String type. 
+// ^ Also called a static method ^
