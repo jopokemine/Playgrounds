b29100ae2dffc609edf3eb689eadb54edc9c08a3
Joshua Reed <JoshuaR@clearswift.com>
2019-11-12 10:45:07 +0000

    Added playground files

---
+++ ruby/playground.rb
@@ -0,0 +1,16 @@
+puts "string" # Put string to console
+print 123 # print something to console
+something = gets # gets from user input
+something = gets.chomp # gets from user input and removed extra new line
+"string".upcase # to upper case
+"STRING".downcase # to lower case
+"string".capitalize # ==> "String"
+"sTrInG\n".chomp.upcase # chomp, then to upper case
+string = "my string"
+"My string is : #{string}" # String interpolation
+string.upcase! # Modifies string variable directly, rather than returning result
+=begin
+multi
+line
+comment
+=end
